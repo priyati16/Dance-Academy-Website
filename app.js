68c4e0c5019722f04dfe303a9fe512513652158a
Priya <priyati16@gmail.com>
2021-09-09 22:21:41 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8000;
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded())
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+
+// ENDPOINTS
+app.get('/', (req, res)=>{
+    const params = {}
+    res.status(200).render('home.pug', params);
+})
+app.get('/contact', (req, res)=>{
+    const params = {}
+    res.status(200).render('contact.pug', params);
+})
+app.get('/about', (req, res)=>{
+    const params = {}
+    res.status(200).render('about.pug', params);
+})
+app.get('/services', (req, res)=>{
+    const params = {}
+    res.status(200).render('services.pug', params);
+})
+app.get('/classinfo', (req, res)=>{
+    const params = {}
+    res.status(200).render('classinfo.pug', params);
+})
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
